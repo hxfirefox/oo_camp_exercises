2c5a551630c66eedae7f6849d238e355b12d2b26
hx <hxfirefox@sina.com>
2016-05-07 21:05:03 +0800

    extract super class

--- src/main/java/Inch.java
+++ src/main/java/Inch.java
@@ -1,27 +1,7 @@
-public class Inch {
-    private int length;
+public class Inch extends Length {
 
     public Inch(int length) {
-        this.length = length;
+        super(length);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Inch inch = (Inch) o;
-
-        return length == inch.length;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return length;
-    }
-
-    public Inch add(Inch other) {
-        return new Inch(this.length + other.length);
-    }
 }
