03b633d338ede2465bf369afc96bbc1031bbfedc
hx <hxfirefox@sina.com>
2016-05-13 23:00:10 +0800

    make all test green

--- src/main/java/Feet.java
+++ src/main/java/Feet.java
@@ -1,7 +1,10 @@
 public class Feet extends Length {
 
+    public static final int FEET_TO_BASE_UNIT = 3;
+
     public Feet(int length) {
         super(length);
+        setAmountOfBaseUnit(FEET_TO_BASE_UNIT);
     }
 
 }
--- src/main/java/Inch.java
+++ src/main/java/Inch.java
@@ -1,7 +1,10 @@
 public class Inch extends Length {
 
+    public static final int INCH_TO_BASE_UNIT = 1;
+
     public Inch(int length) {
         super(length);
+        setAmountOfBaseUnit(INCH_TO_BASE_UNIT);
     }
 
 }
