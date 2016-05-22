50f1afaf2fa3b7bc236084bddbafb225c95b9811
hx <hxfirefox@sina.com>
2016-05-22 13:01:40 +0800

    move all test from feet and yard test to length test

--- src/test/java/github/hxfirefox/lengthunit/LengthTest.java
+++ src/test/java/github/hxfirefox/lengthunit/LengthTest.java
@@ -2,14 +2,92 @@ package github.hxfirefox.lengthunit;
 
 import org.junit.Test;
 
-import static github.hxfirefox.lengthunit.Length.inch;
-import static github.hxfirefox.lengthunit.Length.mile;
-import static github.hxfirefox.lengthunit.Length.yard;
+import static github.hxfirefox.lengthunit.Length.*;
 import static junit.framework.TestCase.assertFalse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class LengthTest {
+    @Test
+    public void should_3_feet_equals_3_feet() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(feet(3).equals(feet(3)), is(true));
+    }
+
+    @Test
+    public void should_3_feet_not_equals_2_feet() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertFalse(feet(3).equals(feet(2)));
+    }
+
+    @Test
+    public void should_5_feet_equals_1_feet_add_4_feet() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(feet(5).equals(feet(1).add(feet(4))), is(true));
+    }
+
+    @Test
+    public void should_1_feet_equals_3_inch() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(feet(1).equals(inch(3)), is(true));
+    }
+
+    @Test
+    public void should_3_yard_equals_3_yard() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(yard(3).equals(yard(3)), is(true));
+    }
+
+    @Test
+    public void should_3_yard_not_equals_2_yard() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertFalse(yard(3).equals(yard(2)));
+    }
+
+    @Test
+    public void should_5_yard_equals_1_yard_add_4_yard() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(yard(5).equals(yard(1).add(yard(4))), is(true));
+    }
+
+    @Test
+    public void should_1_yard_equals_12_feet() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(yard(1).equals(feet(12)), is(true));
+    }
+
     @Test
     public void should_3_mile_equals_3_mile() throws Exception {
         // given
