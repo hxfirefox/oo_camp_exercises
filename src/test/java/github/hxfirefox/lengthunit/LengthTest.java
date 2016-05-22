a48e29ecceb8e6c4688e6180518a78343a6f6f1e
hx <hxfirefox@sina.com>
2016-05-22 12:57:39 +0800

    move all test from inch test to length test

--- src/test/java/github/hxfirefox/lengthunit/LengthTest.java
+++ src/test/java/github/hxfirefox/lengthunit/LengthTest.java
@@ -2,6 +2,7 @@ package github.hxfirefox.lengthunit;
 
 import org.junit.Test;
 
+import static github.hxfirefox.lengthunit.Length.inch;
 import static github.hxfirefox.lengthunit.Length.mile;
 import static github.hxfirefox.lengthunit.Length.yard;
 import static junit.framework.TestCase.assertFalse;
@@ -59,5 +60,33 @@ public class LengthTest {
         assertThat(yard(1762).equals(mile(1).add(yard(2))), is(true));
     }
 
+    @Test
+    public void should_3_inch_equals_3_inch() throws Exception {
+        // given
 
+        // when
+
+        // then
+        assertThat(inch(3).equals(inch(3)), is(true));
+    }
+
+    @Test
+    public void should_3_inch_not_equals_2_inch() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertFalse(inch(3).equals(inch(2)));
+    }
+
+    @Test
+    public void should_5_inch_equals_1_inch_add_4_inch() throws Exception {
+        // given
+
+        // when
+
+        // then
+        assertThat(inch(5).equals(inch(1).add(inch(4))), is(true));
+    }
 }
